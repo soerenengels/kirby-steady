c85dcd5dd91f5b0790c1117fdb7c048ed53ee767
Sören <34400296+soerenengels@users.noreply.github.com>
2023-06-30 14:55:41 +0200

    fix data array

--- snippets/components/steady/plans.php
+++ snippets/components/steady/plans.php
@@ -5,7 +5,7 @@ if(!$plans) return;
 <section id="steady-plans" class="steady__plans">
 	<?php if ($plans): ?>
 	<?php foreach ($plans as $plan) : ?>
-		<?php snippet('components/steady/plan', compact($plan)) ?>
+		<?php snippet('components/steady/plan', ['plan' => $plan]) ?>
 	<?php endforeach ?>
 	<?php else: ?>
 		<!-- Empty State -->
