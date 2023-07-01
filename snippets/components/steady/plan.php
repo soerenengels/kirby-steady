916d50a8aa77e006e8ee733ff354f3ae41d18488
Sören <34400296+soerenengels@users.noreply.github.com>
2023-07-01 13:44:13 +0200

    add translation

--- snippets/components/steady/plan.php
+++ snippets/components/steady/plan.php
@@ -1,9 +1,14 @@
 
 <?php
+use Kirby\Toolkit\I18n;
 $plan = $plan ?? null;
+/* Workaround, see: https://kirby.nolt.io/246 */
+if (option('slugs')) {
+	I18n::$locale = option('slugs');
+}
 if ($plan?->hidden) return; ?>
 <section id="steady-plan" class="steady__plan container">
-	<div class="steady__plan plan__header"><?php $plan->name ?></div>
+	<div class="steady__plan plan__header"><?= $plan->name ?></div>
 	<div class="steady__plan plan__image">
 		<img src="<?= $plan->image_url ?>" alt="">
 	</div>
@@ -17,7 +22,7 @@ if ($plan?->hidden) return; ?>
 		<?= $plan->annual_amount / 100 ?> <?= $plan->currency ?>
 	</div>
 	<div class="steady__plan plan__comparison">
-		Du sparst <?= 1 - ($plan->monthly_amount * 12 - $plan->annual_amount) / $plan->monthly_amount * 12 ?> % im Vergleich zur monatlichen Abrechnung
+		<?= tt('soerenengels.steady.snippets.plan.comparison', null, ['saving' => (1 - ($plan->monthly_amount * 12 - $plan->annual_amount) / $plan->monthly_amount * 12)]) ?>
 	</div>
 	<?php if ($plan->goal_enabled): ?>
 	<div class="steady__plan plan__goal">
