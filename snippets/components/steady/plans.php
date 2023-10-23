3dde6ccf4dd542507817ae5f860a99b5a42aee06
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-23 11:36:25 +0200

    add basic styles and empty state

--- snippets/components/steady/plans.php
+++ snippets/components/steady/plans.php
@@ -9,5 +9,38 @@ if(!$plans) return;
 	<?php endforeach ?>
 	<?php else: ?>
 		<!-- Empty State -->
+		<section>
+			<p>No Plans yet.</p>
+		</section>
 	<?php endif ?>
 </section>
+<style>
+	.steady__plans {
+		display: flex;
+		gap: 1em;
+		font-size: .8em;
+	}
+	.steady__plan.container {
+		flex-basis:14em;
+		flex-grow: 1;
+		flex-shrink: 0;
+		display: grid;
+		grid-template-areas:
+			"name"
+			"image"
+			"benefits";
+		grid-template-rows: auto auto 1fr auto;
+	}
+	.steady__plan.plan__benefits {
+		margin-block: 2em;
+		grid-area: benefits;
+	}
+	.steady__plan.plan__header {
+		grid-area: name;
+		font-weight: 900;
+		font-size: calc(1em / 0.8);
+	}
+	.steady__plan.plan__image {
+		grid-area: image;
+	}
+</style>
