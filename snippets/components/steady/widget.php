e8e0e2df6c874046a3f6a6cfa88d69dc14cd71f3
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-31 13:53:24 +0100

    improve failure proofness

--- snippets/components/steady/widget.php
+++ snippets/components/steady/widget.php
@@ -2,9 +2,7 @@
 /**
  * Add this snippet to the <head>...</head>
  * of your HTML document.
- * Füge dieses Snippet in den <head>...</head>
- * deines HTML-Dokuments ein.
  */
-if (!steady()->widgets()->enabled()) return;
+if (!steady()?->widgets()?->enabled()) return;
 ?>
 <script type="text/javascript" src="<?= steady()->publication()->js_widget_url ?>"></script>
