bb9a99cdcdee337650ddba295cdbe8dc75f5e488
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-31 14:22:07 +0100

    clean up class

--- classes/Steady/User.php
+++ classes/Steady/User.php
@@ -1,35 +1,32 @@
 <?php
 
 namespace Soerenengels\Steady;
+use Kirby\Toolkit\Date;
 
 /**
- * User Class
+ * Generic Steady User Class
  * as object included in Steady API response
  * see https://developers.steadyhq.com/#subscriptions
  *
  * @param array $data associative array
- *
- * @author Sören Engels <mail@soerenengels.de>
- * @version 1.0
- * @see https://github.com/soerenengels/kirby-steady
  */
 class User
 {
 
 	/** @var string user id */
 	public string $id;
-
-	/** @var string type 'user' */
-	public string $type;
-
+	/** @var UserType $type type of user: USER|NEWSLETTER_SUBSCRIBER*/
+	public UserType $type;
 	/** @var string users email */
 	public string $email;
-
 	/** @var string users first name */
 	public string $first_name;
-
 	/** @var string users last name */
 	public string $last_name;
+	/** @var bool $has_password boolean if the user has set a password for their account */
+	public bool $has_password;
+	/** @var Date datetime converted to Kirby\Toolkit\Date when the newsletter subscriber clicked the opt-in link in the email */
+	public Date $opted_in_at;
 
 	/** @var string url for Steady user avatar
 	 * e.g. "https://assets.steadyhq.com/gfx/defaults/user/avatar.png?auto=format&crop=faces&fit=crop&fm=png&h=200&mask=ellipse&w=200"
@@ -37,17 +34,20 @@ class User
 	public string $avatar_url;
 
 	public function __construct(
-		object $data
+		array $data
 	) {
 		[
 			'id' => $id,
 			'type' => $type,
 			'attributes' => $attributes
-		] = $data;
+		] = $data + [null, null, 'attributes' => []]; // + defaults
 		$this->id = $id;
-		$this->type = $type;
+		$this->type = UserType::tryFrom($type);
 		foreach ($attributes as $key => $value) {
 			$key = str_replace('-', '_', $key);
+			if ($key == 'opted_in_at') {
+				$value = new Date($value);
+			}
 			$this->{$key} = $value;
 		};
 	}
