bf41635c630a41e261696e4f896aa457858f2de9
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-31 13:54:03 +0100

    add title and js methods and checkout case

--- classes/Steady/WidgetType.php
+++ classes/Steady/WidgetType.php
@@ -2,10 +2,42 @@
 
 namespace Soerenengels\Steady;
 
+/**
+ * WidgetType Enum
+ * @method string title() title of WidgetType
+ * @method string js() js title of WidgetType
+ * */
 enum WidgetType: string
 {
 	case PAYWALL = 'paywall';
-	case ADBLOCK = 'adblock';
-	case FLOATING_BUTTON = 'floatingButton';
-	// TODO: case CHECKOUT = 'checkout';
+	case ADBLOCK = 'adblock_detection';
+	case FLOATING_BUTTON = 'floating_button';
+	case CHECKOUT = 'checkout';
+
+	/**
+	 * Returns title of WidgetType case
+	 */
+	public function title(): string
+	{
+		return match ($this) {
+			self::PAYWALL => 'Paywall',
+			self::ADBLOCK => 'Adblock Detection',
+			self::FLOATING_BUTTON => 'Floating Button',
+			self::CHECKOUT => 'Checkout'
+		};
+	}
+
+	/**
+	 * Returns identifier of WidgetType case
+	 * for Steady JS Widget Loader string
+	 */
+	public function js(): string
+	{
+		return match ($this) {
+			self::PAYWALL => 'paywall',
+			self::ADBLOCK => 'adblock',
+			self::FLOATING_BUTTON => 'floating_button',
+			self::CHECKOUT => 'checkout'
+		};
+	}
 }
