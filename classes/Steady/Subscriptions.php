b6f8d59dd8105b935e18e83412592acf3811c23f
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-31 14:19:53 +0100

    delete interface, add methods filter count factory

--- classes/Steady/Subscriptions.php
+++ classes/Steady/Subscriptions.php
@@ -2,30 +2,26 @@
 namespace Soerenengels\Steady;
 use Soerenengels\Steady\Subscription;
 
-interface SubscriptionsInterface {
-	public function filter($query): Subscriptions;
-}
-
 /**
  * Steady Subscriptions
  * as requested via the Steady API
  * https://developers.steadyhq.com/#subscriptions
  *
  * @param object $response steady api response
- *
- * @author Sören Engels <mail@soerenengels.de>
- * @version 1.0
- * @see https://github.com/soerenengels/kirby-steady
+ * @method count(): returns int
+ * @method find(string $id): returns ?Subscription
+ * @method filter(\Closure $filter): returns Subscriptions object
+ * @method list(): returns array of Subscription objects
  */
-class Subscriptions implements SubscriptionsInterface
+class Subscriptions
 {
 
-	/** @var array array of Subscription objects */
-	public string $subscriptions = [];
+	/** @var Subscription[] array of Subscription objects */
+	public array $subscriptions = [];
 	/** @var array array of included users */
-	public string $included_users = [];
+	public array $included_users = [];
 	/** @var array array of included plans */
-	public string $included_plans = [];
+	public array $included_plans = [];
 
 	function __construct(
 		array $response
@@ -36,14 +32,71 @@ class Subscriptions implements SubscriptionsInterface
 			$this->subscriptions[] = new Subscription($subscription);
 		};
 
+		// filter included data for plan data
 		$plans = array_filter($included, function($item) {
 			return $item['type'] == 'plan';
 		});
 		$this->included_plans = $plans;
 
+		// filter included data for users data
 		$users = array_filter($included, function($item) {
 			return $item['type'] == 'user';
 		});
 		$this->included_users = $users;
 	}
+
+	/**
+	 * Returns total Subscription objects
+	 */
+	public function count(): int {
+		return count($this->list());
+	}
+
+	/**
+	 * Filters Subscriptions by $attribute and $value
+	 * @param \Closure $filter custom filter function
+	 * @return Subscriptions returns new and filtered Subscriptions object
+	 */
+	public function filter(\Closure $filter): Subscriptions {
+		$filtered_subscriptions = array_filter(
+			$this->list(),
+			$filter
+		);
+		return self::factory($filtered_subscriptions);
+	}
+
+	/**
+	 * Create Subscriptions object from array of Subscription objects
+	 * @param Subscription[] $list array of Subscription objects
+	 * @return Subscriptions
+	 */
+	public static function factory(array $list): Subscriptions {
+		$subscriptions = new Subscriptions([]);
+		$subscriptions->subscriptions = $list;
+		return $subscriptions;
+	}
+
+	/**
+	 * Find Subscription by $id
+	 * @param string $id plan id
+	 * @return ?Subscription returns Subscription or null
+	 */
+	public function find(string $id): ?Subscription {
+		return array_reduce(
+			$this->list(),
+			function (?Subscription $carry, ?Subscription $item) use ($id) {
+				return $carry ?? ($item->id === $id ? $item : $carry);
+			},
+			null
+		);
+	}
+
+	/**
+	 * Returns array of Subscription objects
+	 * @return Subscription[]
+	 */
+	public function list(): array {
+		return $this->subscriptions;
+	}
+
 }
