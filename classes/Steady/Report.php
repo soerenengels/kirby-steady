3a2f61ef388884387bb2bd15e10033bebd56f87e
Sören <34400296+soerenengels@users.noreply.github.com>
2023-10-31 13:46:01 +0100

    doc blocks instead of interface

--- classes/Steady/Report.php
+++ classes/Steady/Report.php
@@ -1,13 +1,9 @@
 <?php
 namespace Soerenengels\Steady;
 
-interface ReportInterface {
-	public function render();
-}
-
 /**
- * @param string $label Each report must have a label
- * @param string $value Each report must have a value
+ * @param string $label required Each report must have a label
+ * @param string $value required Each report must have a value
  * @param string|null $info The info text is shown below the value and is optional. It can be fully customized.
  * @param string|null $theme You can colorize the info value with the theme option. Available themes are:
  * * positive (green)
@@ -15,11 +11,9 @@ interface ReportInterface {
  * * notice (orange)
  * * info (blue)
  * @param string|null $link Reports can have a link to a source or more details. Links can be absolute or relative.
- *
- * @see https://getkirby.com/docs/reference/panel/sections/stats
+ * @method toArray() returns array for Stats report
 */
-
-class Report implements ReportInterface {
+class Report {
 	function __construct(
 		public string $label,
 		public string|int $value,
@@ -29,15 +23,17 @@ class Report implements ReportInterface {
 	) {}
 
 	/**
-	 * returns Object to render Stats report
+	 * Returns array of Report parameters
+	 * enabling to render Stats report
+	 * @see https://getkirby.com/docs/reference/panel/sections/stats
 	 */
-	public function render() {
+	public function toArray(): array {
 		return [
 			'label' => $this->label,
 			'value' => $this->value,
-			'info' => $this->info,
-			'theme' => $this->theme,
-			'link' => $this->link
+			'info' => $this?->info,
+			'theme' => $this?->theme,
+			'link' => $this?->link
 		];
 	}
 }
