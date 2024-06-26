89b1a7c8f29dde4dea3e0cb8fa98821cff72104b
Sören <34400296+soerenengels@users.noreply.github.com>
2024-06-26 15:14:29 +0200

    fix deprecated k-inside

--- src/components/SteadyView.vue
+++ src/components/SteadyView.vue
@@ -1,5 +1,5 @@
 <template>
-	<k-inside>
+	<k-panel-inside>
 		<k-view class="k-steady">
 			<k-header class="k-steady-header">
 				{{ publication.title }}
@@ -297,7 +297,7 @@
 				</k-grid>
 			</template>
 		</k-view>
-	</k-inside>
+	</k-panel-inside>
 </template>
 
 <script>
