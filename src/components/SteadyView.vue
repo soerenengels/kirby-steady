f5dda8e058e0033b8d861d8011185c56ffb47a68
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-18 21:50:01 +0200

    chore: Add prettier config file

--- src/components/SteadyView.vue
+++ src/components/SteadyView.vue
@@ -421,7 +421,7 @@ export default {
 			return `https://steadyhq.com/de/backend/publications/${this.publication.id}/home`;
 		}
 		filteredSubscriptions() {
-			return this.subscriptions.filter((sub) => sub.monthly_amount < 500);
+			return this.subscriptions.filter(sub => sub.monthly_amount < 500);
 		},
 	},
 	methods: {},
