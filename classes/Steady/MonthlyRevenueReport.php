979d118f3e6262cb5b77326f698c74efba41368d
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-18 21:16:48 +0200

    refactor: remove silly revenue judgement

--- classes/Steady/MonthlyRevenueReport.php
+++ classes/Steady/MonthlyRevenueReport.php
@@ -9,15 +9,7 @@ class MonthlyRevenueReport extends Report {
 		$currency = '€';
 		$this->label = t('soerenengels.steady.reports.revenue.label');
 		$this->value = $revenue . '' . $currency;
-		$this->info = (
-			$revenue >=2600 ? t('soerenengels.steady.reports.revenue.info.much') : (
-				$revenue > 520 ? t('soerenengels.steady.reports.revenue.info.more') : (
-					$revenue > 40 ? t('soerenengels.steady.reports.revenue.info.some') : (
-						$revenue > 1 ? t('soerenengels.steady.reports.revenue.info.few') : t('soerenengels.steady.reports.revenue.info.none')
-					)
-				)
-			)
-		);
+		$this->info = null;
 		$this->theme = 'default';
 		$this->link = 'https://steadyhq.com/de/backend/publications/' . $steady->publication()->id . '/analytics';
 		$this->icon = 'money';
