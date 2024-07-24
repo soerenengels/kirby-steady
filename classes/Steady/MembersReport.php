e1047a754ef89da8b67f28c631e817d91edb065a
Sören <34400296+soerenengels@users.noreply.github.com>
2024-07-24 21:49:45 +0200

    feat: Add comparison to MembersReport

--- classes/Steady/MembersReport.php
+++ classes/Steady/MembersReport.php
@@ -10,10 +10,16 @@ class MembersReport extends Report {
 	public function __construct()
 	{
 		$steady = steady();
+		$new_members = count(array_filter($steady->subscriptions()->list(), function(Subscription $item) {
+			return $item->inserted_at->compare()->days <= 30;
+		}));
+		$indicator = $new_members > 0 ? '+' : '±';
+		$info = ($new_members > 0 ? ($new_members > 1 ? $indicator . $new_members . t('soerenengels.steady.reports.members.info.plural') : $indicator . t('soerenengels.steady.reports.members.info.singular')) : t('soerenengels.steady.reports.members.info.zero'));
+		$theme = $new_members > 1 ? 'positive' : ($new_members > 0 ? 'default' : 'default');
 		$this->label = t('soerenengels.steady.reports.members.label');
 		$this->value = $steady->publication()->members_count;
-		$this->info = t('soerenengels.steady.reports.members.info.default'); // TODO: inserted_at compare members with Time X
-		$this->theme = 'info'; // TODO: choose theme based on comparison
+		$this->info = $info;
+		$this->theme = $theme;
 		$this->link = 'https://steadyhq.com/de/backend/publications/' . $steady->publication()->id . '/members';
 		$this->icon = 'users';
 	}
