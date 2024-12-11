45ce5bd1d7a2a7ea6bcb17ff5c6892cd51167649
Sören <34400296+soerenengels@users.noreply.github.com>
2024-12-11 22:28:16 +0100

    add: options for size and utm param in paywall block

--- snippets/blocks/steady/paywall.php
+++ snippets/blocks/steady/paywall.php
@@ -1,2 +1,7 @@
+<?php use Kirby\Toolkit\Html; ?>
 <?php if(!steady()->widgets()->paywall()->isActive()) return; ?>
-<p>___STEADY_PAYWALL___</p>
+<?= Html::tag('div', [
+	'id' => 'steady_paywall',
+	'style' => 'display: none;',
+	'data-utm-campaign' => $block->utm_campaign()->or(''),
+]) ?>
